647883615ed7315aee45cf07f2c881e3e9e9ebfe
Kein <tensaikev@gmail.com>
2023-11-08 00:34:29 -0500

    initial

---
+++ 100DaysOfSwiftUI/Days1-13/1-6/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for _ in platforms {
+    print("hi")
+}
+
+for ind in platforms.indices {
+    print(ind)
+}
+
+for ind in 0..<platforms.count{
+    print(ind)
+}
---
+++ 100DaysOfSwiftUI/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Kein Li on 11/8/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
