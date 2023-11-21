8b6f7f16b37b2ecedc6f3b58b0e3781a4b44f156
Kein <tensaikev@gmail.com>
2023-11-21 11:50:53 -0500

    Unit conversion app finalize

--- UnitConversion/UnitConversion/ContentView.swift
+++ UnitConversion/UnitConversion/ContentView.swift
@@ -13,8 +13,7 @@ enum UnitTypes: String, CaseIterable {
         
     case currency = "Currency"
     case length = "Length"
-//    case temperature = "Temperature"
-//    case time = "Time"
+    case temperature = "Temperature"
 }
 
 enum SubUnitTypes : String, CaseIterable {
@@ -25,32 +24,42 @@ enum SubUnitTypes : String, CaseIterable {
     case meter = "Meter"
     case kilometer = "Kilometer"
     case feet = "Feet"
+    case f = "Farenheit"
+    case c = "Celcius"
+    case k = "Kelvin"
+    
     
     var conversionVal : Int {
         switch self {
-        case .usd, .jpy, .cad :
+        case .usd,.jpy,.cad :
             return 1
-        case .inches, .meter, .kilometer, .feet:
+        case .inches, .meter, .kilometer, .feet :
             return 2
+        case .f, .c, .k :
+            return 3
         }
     }
 }
 
 let unitDict : [UnitTypes : [SubUnitTypes]] = [
     UnitTypes.currency : [.usd,.jpy,.cad],
-    UnitTypes.length : [.inches,.meter,.kilometer,.feet]]
+    UnitTypes.length : [.inches,.meter,.kilometer,.feet],
+    UnitTypes.temperature : [.f,.c,.k]
+]
 
 // Base units : USD for currency, Inches for length
 // If we have CAD to JPY : CAD returns 0.76 USD.
 let conversionDict : [String : Double] = [
     "USD" : 1.0,
-    "JPY" : 0.0091,
-    "CAD" : 0.76,
+    "JPY" : 0.0067,
+    "CAD" : 0.7288,
     "Inches" : 1.0,
     "Meter" : 39.37,
     "Feet" : 12,
-    "Kilometer" : 39370.1
-
+    "Kilometer" : 39370.1,
+    "Farenheit" : 1.0,
+    "Kelvin" : -457.87,
+    "Celcius" : 33.8
 ]
 
 
@@ -62,12 +71,16 @@ struct ContentView: View {
     @State private var toUnit : SubUnitTypes = SubUnitTypes.usd
     
     @State private var inputVal = ""
-    @State private var outputVal = ""
+    @State private var outputVal : Double?
     
-    private var inputDouble : Double {
-        return Double(inputVal) ?? 0.0
+    @FocusState private var focused : Bool
+    
+    
+    private var inputDouble : Double? {
+        return Double(inputVal) ?? nil
     }
     
+    // A computed property that will update the two unit pickers based on selection
     private var selectedSubArr : [SubUnitTypes] {
         return unitDict[currentUnitSelection]!
     }
@@ -85,7 +98,7 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
-                Section {
+                Section("From") {
                     VStack{
                         Picker("", selection: $fromUnit) {
                             ForEach(selectedSubArr, id : \.self) { val in
@@ -97,9 +110,10 @@ struct ContentView: View {
                     
                     TextField("Enter input: ", text: $inputVal)
                         .keyboardType(.decimalPad)
+                        .focused($focused)
                 }
                 
-                Section {
+                Section("To") {
                     Picker("", selection: $toUnit) {
                         ForEach(selectedSubArr, id : \.self) { val in
                             Text(val.rawValue)
@@ -108,49 +122,83 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                     
                     
-                    Button("convert") {
+                    HStack{
+                        Text("Output:")
+                        Spacer()
+                        Text("\(outputVal ?? 0.0 , specifier : "%.2f")")
+                    }
+                }
+                
+                HStack {
+                    Button("Convert") {
                         outputVal = conversion(fromUnit,toUnit,inputDouble)
                     }
-                   
-                    Button("test") {
-                        print(selectedSubArr)
-                        print(fromUnit.rawValue + " " + toUnit.rawValue)
-                    }
+                    .buttonStyle(.borderless) // needed to add this otherwise both buttons were being pressed
                     
-                    Text(outputVal)
+                    Spacer()
+                    Button("Reset") {
+                        fromUnit = SubUnitTypes.cad
+                        toUnit = SubUnitTypes.usd
+                        inputVal = ""
+                    }
+                    .foregroundColor(.red)
+                    .buttonStyle(.borderless) // needed to add this otherwise both buttons were being pressed
+
+
                 }
                
             }
+            .navigationTitle("Conversion App")
+            .toolbar {
+                if focused {
+                    Button("Done"){
+                        focused = false
+                    }
+                }
+            }
+            
         }
         .padding()
         
     }
 }
 
-func conversion (_ from : SubUnitTypes , _ to : SubUnitTypes, _ inputVal : Double) -> String {
+// By using enums, we can pass in the two types and then just use their raw values
+// Raw values are then keys in the dictionary
+func conversion (_ from : SubUnitTypes , _ to : SubUnitTypes, _ inputVal : Double?) -> Double? {
     
-    // Use guard to unwrap. Return if either is invalid
-    guard let val1 = conversionDict[from.rawValue], let val2 = conversionDict[to.rawValue] else {
-        return ""
+    
+    guard let baseConversionVal = conversionDict[from.rawValue] else {
+        return nil
+    }
+    if (inputVal == nil){
+        return nil
     }
     
-    if val1 == val2 {
-        return String(inputVal)
+    
+    var toBase = baseConversionVal * inputVal!
+    switch to {
+    case .jpy :
+        return toBase * 148.35
+    case .cad :
+        return toBase * 1.372
+    case .usd :
+        return toBase * 1.0
+    case .inches:
+        return toBase * 1.0
+    case .feet :
+        return toBase * 12
+    case .meter :
+        return toBase * 39.37
+    case .kilometer:
+        return toBase * 39370.0787
+    case .f:
+        return toBase * 1.0
+    case .c:
+        return (toBase - 32) * (5/9)
+    case .k:
+        return (toBase + 459.67) * (5/9)
     }
-    
-    var intVal1 = from.conversionVal
-    var result = inputVal * val1 / val2
-    return String(result)
-    
-//    switch intVal {
-//        // Means currency, so convert from to the base type
-//    case 1 :
-//
-//
-//        // Means units
-//    case 2 :
-
-//    }
 }
 
 
